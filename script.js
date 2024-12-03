9e7418b55a64aac1b5737093ef179166c5665105
zeusvikey <127013256+zeusvikey@users.noreply.github.com>
2024-12-03 22:46:53 +0530

    resume

---
+++ script.js
@@ -0,0 +1,12 @@
+function addNewWEFIELD(){
+    console.log("Adding new Filed");
+    let newNode=document.createElement('textarea');
+    newNode.classList.add('form-control');
+    newNode.classList.add('weField');
+    newNode.setAttribute("rows",3);
+
+
+    let weOb=document.getElementById("we");
+    let weAddButtonOb=document.getElementById ("weAddButton");
+    
+}
